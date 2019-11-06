3410a8bd37cfb50f0602472d7c8ebc85a4330491
IJNmiko <IJNmiko@protonmail.ch>
2019-11-06 17:45:39 +1300

    Conform to flake8 standards

--- Game.py
+++ Game.py
@@ -1,13 +1,14 @@
 from PIL import Image
 
+
 class Game:
     def __init__(self):
         self.maps = {}
         self.map_size = 40
 
         self.current_map = 'start'
-        self.player_x    = int(self.map_size / 2)
-        self.player_y    = int(self.map_size / 2)
+        self.player_x = int(self.map_size / 2)
+        self.player_y = int(self.map_size / 2)
 
         self.tiles = {
             'ground': ' ',
@@ -18,8 +19,9 @@ class Game:
     def playerMove(self, relative_x, relative_y):
         new_x = self.player_x + relative_x
         new_y = self.player_y + relative_y
+        index = (new_y * self.map_size) + new_x
 
-        if self.maps[self.current_map][(new_y * self.map_size) + new_x] != 'ground':
+        if self.maps[self.current_map][index] != 'ground':
             return
 
         self.player_x = new_x
@@ -35,11 +37,11 @@ class Game:
             for x in range(im.size[0]):
                 pixel = im.getpixel((x, y))
 
-                if pixel == (0,0,0):
+                if pixel == (0, 0, 0):
                     tiles.append('ground')
-                elif pixel == (255,255,255):
+                elif pixel == (255, 255, 255):
                     tiles.append('wall')
-                elif pixel == (195,195,195):
+                elif pixel == (195, 195, 195):
                     tiles.append('gate')
                 else:
                     tiles.append('?')
@@ -54,7 +56,7 @@ class Game:
         y = 0
 
         for t in self.maps[self.current_map]:
-            console.draw_char(x, y, self.tiles[t], bg=None, fg=(255,255,255))
+            console.draw_char(x, y, self.tiles[t], bg=None, fg=(255, 255, 255))
 
             x += 1
             if x == self.map_size:
@@ -62,4 +64,10 @@ class Game:
                 y += 1
 
     def renderEntities(self, console):
-        console.draw_char(self.player_x, self.player_y, '@', bg=None, fg=(255,255,255))
+        console.draw_char(
+            self.player_x,
+            self.player_y,
+            '@',
+            bg=None,
+            fg=(255, 255, 255)
+        )
--- castle_venture.py
+++ castle_venture.py
@@ -6,7 +6,14 @@ SCREEN_HEIGHT = 50
 LIMIT_FPS = 20
 
 tdl.set_font('assets/arial10x10.png', greyscale=True, altLayout=True)
-console = tdl.init(SCREEN_WIDTH, SCREEN_HEIGHT, title='Castle Venture', fullscreen=False)
+
+console = tdl.init(
+    SCREEN_WIDTH,
+    SCREEN_HEIGHT,
+    title='Castle Venture',
+    fullscreen=False
+)
+
 tdl.set_fps(LIMIT_FPS)
 
 game = Game.Game()
